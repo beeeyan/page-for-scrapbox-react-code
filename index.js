6cd6daaf604141e91bf50d6b4836bf2f0aa96206
sakasanokuma <moonsunphoenix@gmail.com>
2019-12-14 18:38:09 +0900

    順番が関係あるかもしれないので修正

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function loadScript(url) {
     return new Promise(
         (resolve) => {
             const script = document.createElement('script');
+            script.type = 'text/babel'
             script.src = url;
             script.onload = resolve;
-            script.type = 'text/babel'
             document.body.appendChild(script);
         })
 }
